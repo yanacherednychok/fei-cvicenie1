84be8c2ea51ff09920f8bad6c77c5e897cf21fa6
Yana Cherednychok <xcherednychok@stuba.sk>
2023-02-22 08:51:31 +0100

    new

---
+++ sdcfg/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        test1 ("Hello world!");
+    }
+    public static void test1(String str){
+        System.out.println(str);
+    }
+}
