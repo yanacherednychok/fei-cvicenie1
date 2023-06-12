497f82d0cb4985ed5901f3db4a7739a903945a67
chabaniuk-m <chabanyuk.maks@gmail.com>
2023-06-12 21:10:28 +0300

    second submit

--- main/src/Main.java
+++ main/src/Main.java
@@ -1,9 +1,12 @@
+import shape.Circle;
 import shape.Triangle;
 
 public class Main {
     public static void main(String[] args) {
         Triangle triangle = new Triangle();
         System.out.println(triangle);
+        Circle circle = new Circle();
+        System.out.println(circle);
         System.out.println("Hello world!");
         System.out.println("Hello world!");
         System.out.println("Hello world!");
